d48b48a29ead6f60f149cd801f9e7e2509456a0d
App Academy Student <student@appacademy.io>
2016-03-11 17:01:42 -0800

    created tictactoenode

--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,19 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    child_mover_mark = get_next_mark
+
+    positions = [[0,0],[0,1],[0,2][1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
+    positions.each do |pos|
+      unless @board[pos]
+        new_board = @board.dup
+        new_board[pos] = child_mover_mark
+        new_node = TicTacToeNode.new(new_board, child_mover_mark, pos)
+      end
+    end
+  end
+
+  def get_next_mark
+    @next_mover_mark == :x ? :o : :x
   end
 end
